f002f506e58e178096714b9f33ad96b4b0c3c219
AzmiRayana <azmirayana559@gmail.com>
2024-05-04 16:40:13 +0700

    initial configuration

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//ini javascript
+//ini javascript//
+
+function validation() {
+  console.log("Good");
+}
+
+document
+  .getElementById("home-btn")
+  .addEventListener("click", () => validation());
+
+document
+  .getElementById("our-package-btn")
+  .addEventListener("click", () => validation());
+
+document
+  .getElementById("call-us-btn")
+  .addEventListener("click", () => validation());
